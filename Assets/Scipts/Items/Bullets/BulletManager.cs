d0dde5908ad3b1305378a0251eec90a39425dffb
EvolveVR <Tyler.Heers@gmail.com>
2016-06-18 19:28:08 -0700

    Additional Folder Structure
    1) added additional folder structure according to overall gun
    classifications
    2) added a folder for future diagrams of the code

---
+++ Assets/Scipts/Items/Bullets/BulletManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Hydrogen;
+public class BulletManager : MonoBehaviour
+{
+    
+    //We'll be passing in the derived classes of gun as keys, then the bullet pools as values
+    Dictionary<Gun, List<GameObject>> allBulletPools;
+
+	void Awake()
+    {
+        
+        allBulletPools = new Dictionary<Gun, List<GameObject>>();  
+    }
+	void Start ()
+    {
+     //We'll add in the other types of bulletPools in here 
+       allBulletPools.Add(GameObject.Find("PM-40").GetComponent<SemiAutomaticGun>(),gameObject.GetComponent<HandGunBulletPool>().getPool);
+
+        //This was testing to make sure different derived gun types worked as keys and they did.
+   //  allBulletPools.Add(GameObject.Find("tempgun").GetComponent<AutomaticGun>(), gameObject.GetComponent<HandGunBulletPool>().getPool);
+	}
+	
+	public GameObject chooseBullets(int bulletsShot, Gun currentBullets)
+    {
+        //number of bulletsShot will increment before calling this function, so doing decrementing it since arrays start at 0
+
+        //Returns the bulletPool for right gun
+
+        
+        return allBulletPools[currentBullets][bulletsShot-1];
+
+    }
+	
+}
