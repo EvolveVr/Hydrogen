8230283600ace5c44269886a44e29764118914e2
Brian Zhao <brianzha@gmail.com>
2016-06-28 03:16:36 -0700

    Projectiles
    Added Projectile interface.  Bare bones right now, all properties in
    Projectile are outlined in the Design Document.
    Made Bullet implement the Projectile interface.

---
+++ Assets/Scipts/Items/Bullets/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Interface for all items that deal damage to targets
+/// </summary>
+public interface Projectile{
+
+    float penetration { set; }
+    //value - amount to reduce penetration by
+
+    Vector3 position { get; }
+    //returns the position of acting point on the projectile
+    
+}
