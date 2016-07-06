2f831fbfc95485cc7851a1c0bb1f49c2fd0ad615
rj45toCerebrum <Tyler.Heers@gmail.com>
2016-07-06 15:17:34 -0700

    Charge Gun Slider
    Added:
    1) Slider to visualize the charging of the charge gun
    2) added a ChargeGunSlider class that handles the visualization of
    charge gun sliders

--- Assets/Scipts/Items/Weapons/Projectile/Guns/ChargeGun.cs
+++ Assets/Scipts/Items/Weapons/Projectile/Guns/ChargeGun.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 namespace Hydrogen
 {
+    /// <summary>
+    /// This is a basic class for Guns that Charge.
+    /// </summary>
     public class ChargeGun : Gun
     {
         //A gun that charges needs 1) max charge time, 2) and the current charge time
@@ -11,23 +14,29 @@ namespace Hydrogen
         public float maxChargeTime = 2.0f;
         private float _chargeTime = 0f;
 
+        #region Properties
+        //we need to be able to access outside this class; ie for updating the slider value UI element
+        public float chargeTime { get { return _chargeTime; } }
+        #endregion
+
+        #region Unity Events
         protected override void Update()
         {
             base.Update();
-
-
         }
 
+        #endregion
+
+        #region Overloaded NVR Methods
+        // Overload the NVR Button pressed. WHen button is being ressed we need to add to the time and keep it in a certain range
         public override void UseButtonPressed()
         {
             _chargeTime += Time.deltaTime;
 
-            if (_chargeTime >= maxChargeTime)
-            {
-                _chargeTime = maxChargeTime;
-            }
+            _chargeTime = Mathf.Clamp(_chargeTime, 0.0f, maxChargeTime);
         }
 
+        //Only shoot the gun when the button is released. Hence the overload
         public override void UseButtonUp()
         {
             //only shoot when it is charged enough
@@ -36,8 +45,13 @@ namespace Hydrogen
                 shootGun();
             }
 
+            // Everytime the trigger is released, we need to reset the timer
             _chargeTime = 0f;
         }
+
+        #endregion
+
+
     } // end of Charge class
 
 }
