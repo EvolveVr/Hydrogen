f8d28372bf8f0820e99e7bb9d7e7762ac19371e6
Brian Zhao <brianzha@gmail.com>
2016-07-08 17:49:03 -0700

    Anchor/Target Framework
    Added in functions for Anchor, Target, and TargetMovementManager.

---
+++ Assets/Scipts/Target/Anchor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Hydrogen {
+
+    public class Anchor : MonoBehaviour
+    {
+        //List of targets to spawn
+        private Target[] _targetList;
+        //difficulty level of the anchor
+        private int _difficulty;
+
+        // Use this for initialization
+        void Start()
+        {
+            //TODO: Spawn targets
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //TODO: move anchor in a simple
+        }
+
+        public void setTargetList(Target[] newList)
+        {
+            _targetList = newList;
+        }
+
+        public void setInitial(Transform initialPosition, Vector3 initialMovement,Movement movementPattern)
+        {
+
+        }
+
+
+
+    }
+}
