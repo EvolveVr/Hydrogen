4b3268df8a69e8ce919384fe14a1832c9eb2e3d8
rj45toCerebrum <Tyler.Heers@gmail.com>
2016-07-15 19:57:52 -0700

    switch branches

---
+++ Assets/Trash/RestrictedMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using NewtonVR;
+
+
+public class RestrictedMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public NVRHand hand;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(hand.Controller != null)
+        {
+            if(hand.Controller.GetHairTrigger())
+            {
+                Vector3 vectTowardController = (hand.transform.position - this.transform.position);
+                Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, hand.transform.position.z);
+                transform.position = newPosition;
+            }
+        }
+        
+    }
+	
+}
