0287df347c873b57ff173f0173c6aee0d2520331
rj45toCerebrum <Tyler.Heers@gmail.com>
2016-08-05 14:02:38 -0700

    new MainDevelop
    new MainDevelop

---
+++ Assets/Resources/InfinitaStudio - Sci-fi structures and weapons/scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate(new Vector3 (0, 50, 0) * Time.deltaTime);
+	
+	}
+}
