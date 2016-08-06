2b4c5a36929980f9824792e9f9fa72e4e3824841
rj45toCerebrum <Tyler.Heers@gmail.com>
2016-08-06 01:11:31 -0700

    Added logic for Target Class
    Deprecated the Inner, Middle and Outer target classes because i realized there was a better way. Deleted many assets that were not needed.

--- Assets/Scipts/Items/Target/Target.cs
+++ Assets/Scipts/Items/Target/Target.cs
@@ -14,15 +14,52 @@ namespace Hydrogen
         public GameObject particleEffect;
         protected AudioSource targetAudioSource;
 
+        //How much points to give based on where they hit; play with values then make private const
+        public int centerHitPoints = 10;
+        public int middleHitPoints = 5;
+        public int outerHitPoints = 2;
+        [HideInInspector]
+        public int multiplierValue = 0;     // Make sure to add logic for multiplier values later
 
+        // need these for cgiving proper points
+        private float _innerSqrMagnitude;
+        private float _outerSqrMagnitude;
+
+        #region Unity Methods
         void Awake()
         {
             targetAudioSource = GetComponent<AudioSource>();
 
             if (targetAudioSource == null)
                 Debug.LogError("The targets audio source was not found");
+
+            InitMagnitudes();
         }
 
+        void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.tag == "Bullet")
+            {
+                InitParticleEffect(GameConstants.TargetPart.Inner);
+
+                ContactPoint collision = other.contacts[0];
+                Vector3 pos = collision.point;
+                Vector3 fromParentToContactVector = (pos - this.transform.position);
+
+                int pointsToGive = CalcPointsByHitVector(fromParentToContactVector, _innerSqrMagnitude, _outerSqrMagnitude);
+                
+                if(pointsToGive != 1)
+                {
+                    GameManager.gameManager.AddPoints(pointsToGive);
+                }
+
+                Debug.DrawRay(this.transform.position, fromParentToContactVector, Color.cyan, 40.0f);
+                Destroy(transform.parent.gameObject);
+            }
+        }
+        #endregion
+
+        #region Methods
         protected void PlayTargetDestroyAudio()
         {
             targetAudioSource.Play();
@@ -45,5 +82,43 @@ namespace Hydrogen
                     break;
             }
         }
+
+        // used to see if inner middle or outer parts of targets were hit; More efficient to not calculate every hit
+        void InitMagnitudes()
+        {
+            foreach(Transform child in transform.parent)
+            {
+                if(child.name == "InnerTransform")
+                {
+                    _innerSqrMagnitude = (this.transform.position - child.transform.position).sqrMagnitude;
+                }
+                else if(child.name == "OuterTransform")
+                {
+                    _outerSqrMagnitude = (this.transform.position - child.transform.position).sqrMagnitude;
+                }
+            }
+        }
+
+        int CalcPointsByHitVector(Vector3 vectorToContactPoint, float innerSqrMag, float outerSqrMag)
+        {
+            int pointsToGive = -1;
+            float sqrMagnitude = vectorToContactPoint.sqrMagnitude;
+
+            if(sqrMagnitude <= innerSqrMag)
+            {
+                pointsToGive = centerHitPoints;
+            }
+            else if(sqrMagnitude > innerSqrMag && sqrMagnitude < outerSqrMag)
+            {
+                pointsToGive = middleHitPoints;
+            }
+            else
+            {
+                pointsToGive = outerHitPoints;
+            }
+
+            return pointsToGive;
+        }
+        #endregion
     }
 }
